f844ae42a97f061be8825ab87243034d5ef903c4
Susmitha Sundar <susmitha.sundar@syncfusion.com>
2020-07-09 17:43:19 +0530

    How to work with checkboxes in unbound mode in wpf treeview

---
+++ SfTreeViewUnboundCheckBox/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Syncfusion.UI.Xaml.TreeView;
+using Syncfusion.UI.Xaml.TreeView.Engine;
+using Syncfusion.Windows.Shared;
+
+namespace TreeNodeWithCheckBoxDemo
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        #endregion
+    }
+}
